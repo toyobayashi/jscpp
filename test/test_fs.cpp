27081d78229dcaef733f79c320276eea5941fa11
toyobayashi <lifenglin314@outlook.com>
2021-03-23 10:54:37 +0800

    test fs

--- test/test_fs.cpp
+++ test/test_fs.cpp
@@ -22,6 +22,34 @@ TEST(jscppFilesystem, statAndLstat) {
 
   fs::Stats stats2 = fs::lstat(__dirname);
   EXPECT_TRUE(stats2.isDirectory());
+
+#ifndef __EMSCRIPTEN__
+  EXPECT_TRUE(fs::stat(__filename).isFile());
+  EXPECT_TRUE(fs::lstat(__filename).isFile());
+#endif
+
+  fs::symlink(__dirname, "slk3", fs::SymlinkType::ST_JUNCTION);
+  EXPECT_TRUE(fs::exists("slk3"));
+  fs::remove("slk3");
+  EXPECT_FALSE(fs::exists("slk3"));
+#ifndef __EMSCRIPTEN__
+  fs::symlink(__filename, "slk");
+  console.log(fs::realpath("slk"));
+  EXPECT_TRUE(fs::exists("slk"));
+  fs::Stats stat = fs::stat("slk");
+  fs::Stats stat2 = fs::lstat("slk");
+  console.log(stat.size);
+  console.log(stat.isSymbolicLink());
+  console.log(stat2.size);
+  console.log(stat2.isSymbolicLink());
+  fs::remove("slk");
+  EXPECT_FALSE(fs::exists("slk"));
+#endif
+
+  fs::symlink("notexists", "slk2");
+  EXPECT_TRUE(fs::exists("slk2"));
+  fs::remove("slk2");
+  EXPECT_FALSE(fs::exists("slk2"));
 }
 
 TEST(jscppFilesystem, readdir) {
@@ -69,4 +97,56 @@ TEST(jscppFilesystem, mkdirs) {
   console.log(fs::exists(mkdir4));
   console.log(fs::readdir(mkdir3));
 #endif
+}
+
+TEST(jscppFilesystem, copy) {
+  String s = L"testwrite.txt";
+  fs::writeFile(s, "666");
+  auto d = path::basename(__filename + L".txt");
+  fs::copyFile(s, d);
+  fs::remove(s);
+
+  EXPECT_TRUE(fs::exists(d));
+  JSCPP_EXPECT_THROW(fs::copyFile(s, d, true), "");
+  fs::remove(d);
+  EXPECT_FALSE(fs::exists(d));
+  fs::remove(s);
+  EXPECT_FALSE(fs::exists(s));
+
+  JSCPP_EXPECT_THROW(fs::copyFile("notexist", "any"), "");
+
+  fs::mkdirs("./tmp/mkdir/a/b/c");
+  fs::writeFile("./tmp/mkdir/a/b/c.txt", "1\n");
+  fs::writeFile("./tmp/mkdir/a/b.txt", "1\n");
+  fs::copy("./tmp/mkdir", "./tmp/copy");
+  EXPECT_TRUE(fs::exists("./tmp/copy"));
+  JSCPP_EXPECT_THROW(fs::copy("./tmp/mkdir", "./tmp/copy", true), "");
+  JSCPP_EXPECT_THROW(fs::copy("./tmp/mkdir", "./tmp/mkdir/subdir"), "");
+  // try {
+  //   fs::copy("./tmp/mkdir", "./tmp/mkdir/subdir");
+  //   return -1;
+  // } catch (const std::exception& e) {
+  //   console::error(e.what());
+  // }
+
+  fs::remove("./tmp");
+  EXPECT_FALSE(fs::exists("./tmp"));
+}
+
+TEST(jscppFilesystem, readAndWrite) {
+  String data = process.platform + L"测试\r\n";
+  String append = L"append";
+
+  EXPECT_NO_THROW(fs::writeFile("testwrite.txt", data));
+
+
+  EXPECT_EQ(fs::readFileAsString("testwrite.txt"), data);
+  fs::appendFile("testwrite.txt", append);
+  EXPECT_EQ(fs::readFileAsString("testwrite.txt"), (data + append));
+  fs::remove("testwrite.txt");
+
+  fs::mkdirs("testmkdir");
+  JSCPP_EXPECT_THROW(fs::readFileAsString("testmkdir"), "");
+  fs::remove("testmkdir");
+  JSCPP_EXPECT_THROW(fs::readFileAsString("notexists"), "No such file or directory");
 }
